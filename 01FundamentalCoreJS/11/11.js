211f75904103e73f48d06a7a83ffbf4b10d44089
karmazin <kubartphil@gmail.com>
2021-06-07 16:30:15 +0300

    01 Part 11

---
+++ 01FundamentalCoreJS/11/11.js
@@ -0,0 +1,12 @@
+ const First_name = "Sasha"
+
+ let a = First_name.length
+ a 
+ let b = First_name[2]
+b
+let c = First_name[First_name.length -1]
+c
+
+const sl = 'hi Sasha'
+d = sl.split(' ')[1]
+console.log("'" + d + "'")
