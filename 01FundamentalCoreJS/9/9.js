f646487eb2111ab75ae88f2cd82e2fc5befb18a4
karmazin <kubartphil@gmail.com>
2021-06-07 16:10:56 +0300

    01 Part 9

---
+++ 01FundamentalCoreJS/9/9.js
@@ -0,0 +1,19 @@
+ const  nam1 = 10;
+ const  nam2 = 20;
+
+let value;
+
+value = nam1 + nam2 
+// остаток от деления 
+value = 4 % 2
+value = 0.6 + 0.7
+a = value.toFixed(2)
+console.log(value); 
+console.log(a);  
+console.log(typeof a, a);
+b = parseFloat(value.toFixed(2))
+console.log(typeof b, b);
+
+value = (0.6 * 10 + 0.7 * 10)/ 10
+console.log(value); 
+
